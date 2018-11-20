dd8d558fd414d9b23fa9065de5b91e4c6d2e3d53
ajcasado <dekkard.rick@gmail.com>
2018-11-20 12:34:09 +0100

    doLine

--- src/ardvga.h
+++ src/ardvga.h
@@ -218,6 +218,7 @@ class ardvga{
     static volatile uint16_t scanLine;
     static volatile uint16_t hLine;
     static volatile uint8_t drawLine;
+    static volatile uint8_t doLine;
     static uint16_t sndFreq;
     static uint16_t vFrontPorch;
     static uint8_t skipLine;

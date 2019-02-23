f132633be2abf3e50f0cd52a704185723a84316f
antonio <dekkard.rick@gmail.com>
2019-02-23 14:21:18 +0100

    create sendSPI function on master

--- examples/spi_communication/spi_master/spi_master.ino
+++ examples/spi_communication/spi_master/spi_master.ino
@@ -9,19 +9,21 @@
 #define START_TIMER() TCNT1 = 0 ; TCCR1B |= (1 << CS12) | (1 << CS10)   // enable timer with 1024 prescaler, clear counter.
 #define STOP_TIMER() TCCR1B &= ~(1 << CS12) & ~(1 << CS10) // disable timer.
 #define nop() __asm__("nop\n\t") //Wait one clock cycle
+#define PIN_MOSI 11
+#define PIN_MISO 12
+#define PIN_MOSI 13
 
 volatile uint8_t timeoutflag = 0;
-char buff[MAX_BUFLEN] = {0};
-uint8_t buflen , i;
+
 
 void setup(){
   pinMode (PIN_SS , OUTPUT);
-  pinMode (13 , OUTPUT);
-  pinMode (12 , INPUT);
-  pinMode (11, OUTPUT);
-  digitalWrite(PIN_SS,HIGH);
-  digitalWrite(11,HIGH);
-  digitalWrite(13,HIGH);
+  pinMode (PIN_SCK , OUTPUT);
+  pinMode (PIN_MOSI , OUTPUT);
+  digitalWrite(PIN_SS , HIGH);
+  digitalWrite(PIN_MOSI , HIGH);
+  digitalWrite(PIN_MOSI , HIGH);
+  pinMode (PIN_MISO , INPUT);
   pinMode (PIN_RTR , INPUT);
   Serial.begin(115200);
   //set timer1 interrupt at 1Hz -amandaghassaei https://www.instructables.com/id/Arduino-Timer-Interrupts/
@@ -34,56 +36,68 @@ void setup(){
   TCCR1B |= (1 << WGM12);//CTC mode
   TIFR1 |= (1<<OCF1A); //clear interrupt flag
 }
-void loop(){ //hacer función para transferencia y dejar en loop solamente entrada de texto y llamada a función
-  ocioso:
-    SET_SS_HIGH();
-    STOP_TIMER();
-    i = 0;
-    buflen = 0;
-    Serial.print(F("Insert a string of at most "));
-    Serial.print(MAX_BUFLEN , DEC);
-    Serial.println(F(" characters:"));
-    while (!Serial.available());
-    while ((Serial.available()) && (buflen < MAX_BUFLEN)) {
-      buff[buflen++] = Serial.read();
-    }
-    buff[buflen] = 0;
-    Serial.print(F("Sending:"));
-    Serial.println(buff);
-    Serial.println(buflen);
-    SPI.beginTransaction(SPISettings (250000 , MSBFIRST , SPI_MODE3));
-    SET_SS_LOW();
-    START_TIMER();
-  mando_byte:
-    while (GET_RTR_VALUE() && !timeoutflag);
-    if (timeoutflag) goto timeout;
-//    SET_SS_HIGH();//this resets the SPI buffer on the slave to synchronize byte transfer
-//    nop();
-//    nop(); //Keep SS HIGH for 4 cycles to make effect on slave
-//    SET_SS_LOW();
-    cli(); //avoid delay setting SS HIGH
+void loop(){
+  char buff[MAX_BUFLEN] = {0};
+  uint8_t buflen , error;
+  SET_SS_HIGH();//redundant but safe
+  STOP_TIMER();//redundant but safe
+  i = 0;
+  buflen = 0;
+  Serial.print(F("Insert a string of at most "));
+  Serial.print(MAX_BUFLEN , DEC);
+  Serial.println(F(" characters:"));
+  while (!Serial.available());
+  while ((Serial.available()) && (buflen < MAX_BUFLEN)) {
+    buff[buflen++] = Serial.read();
+  }
+  buff[buflen] = 0;
+  Serial.print(F("Sending:"));
+  Serial.println(buff);
+  Serial.println(buflen);
+  error = sendSPI(buff , buflen);
+  switch (error){
+    case 0:
+      Serial.print(F(">OK!"));
+      break;
+    case 1:
+      Serial.print(F(">TIMEOUT!"));
+      break;
+    default:
+      Serial.print(F(">unknown error"));
+  }
+}
+uint8_t sendSPI (char *buff , uint8_t buflen){
+  uint8_t i = 0;
+  SET_SS_LOW();
+  START_TIMER();
+  SPI.beginTransaction(SPISettings (250000 , MSBFIRST , SPI_MODE3));//250kHz tested as max usable SPI frequency for slave to catch clock transitions
+  for (;;){ // actual sending loop
+    while (GET_RTR_VALUE() && !timeoutflag);// wait for slave to be ready
+    if (timeoutflag) break;//check timeout on waiting slave to be ready
+  //    SET_SS_HIGH();//this resets the SPI buffer on the slave to synchronize byte transfer
+  //    nop();
+  //    nop(); //Keep SS HIGH for 4 cycles to make effect on slave
+  //    SET_SS_LOW();
     SPI.transfer(buff[i++]); //I guess from SPI library source that this is a blocking transfer
-    if (--buflen == 0){
-      SET_SS_HIGH();
-      sei();
-      SPI.endTransaction();
-      goto ocioso;
+    cli(); //disable interrupts to avoid extra delay at setting SS HIGH as this could let slave waiting until timepout
+    if (--buflen == 0){// check that sending buffer is empty
+      SET_SS_HIGH(); // tell slave we're done
+      sei(); //reenable interrupts
+      STOP_TIMER(); //stop timeout timer counter
+      SPI.endTransaction();//release SPI port
+      return 0;//return with 0 error code (no error)
     }
-    sei();
-//    SET_SS_HIGH();//this resets the SPI buffer on the slave to synchronize byte transfer
-//    nop();
-//    nop(); //Keep SS HIGH for 4 cycles to make effect on slave
-//    SET_SS_LOW();
-    while (!GET_RTR_VALUE() && !timeoutflag);
-//    while (GET_RTR_VALUE() && !timeoutflag);
-    if (timeoutflag) goto timeout;
-    goto mando_byte;
-  timeout:
-    SPI.endTransaction();
-    Serial.println(F("TIMEOUT!"));
-    timeoutflag = 0;
-    goto ocioso;
+    sei();//reenable interrupts
+    while (!GET_RTR_VALUE() && !timeoutflag);//wait slave ready for next buffer element
+    if (timeoutflag) break;//check timeout on waiting slave to be ready
+}
+SPI.endTransaction(); // only can reach this point because of timeout, release SPI port
+SET_SS_HIGH();//tell slave not sending anything more
+STOP_TIMER();//stop timeout timer counter
+Serial.println(F("TIMEOUT!"));
+timeoutflag = 0; //reset timeout flag
+return 1;//return with 1 error code (timeout)
 }
 ISR (TIMER1_COMPA_vect){
-  timeoutflag =1;
+  timeoutflag = 1; //set timeout flag
 }
